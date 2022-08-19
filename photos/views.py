498b281f5a957183ccdd7523397ed4d8fe713bc0
Thuotracy <tracyjacobs775@gmail.com>
2022-08-19 21:17:08 +0300

    created views for gallery, viewPhoto and addPhoto

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def gallery(request):
+    return render(request, 'photos/gallery.html')
+
+def viewPhoto(request):
+    return render(request, 'photos/photo.html')
+
+def addPhoto(request):
+    return render(request, 'photos/add.html')        
