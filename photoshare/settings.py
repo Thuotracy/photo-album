275268f9baf81b67cd8b9aed12553e71718f4c68
Thuotracy <tracyjacobs775@gmail.com>
2022-08-30 12:41:04 +0300

    created a images folder

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -124,6 +124,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+MEDIA_ROOT = BASE_DIR / 'static/images'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
