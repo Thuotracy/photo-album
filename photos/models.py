51b42fc16ba9b22904b0c4ac6a6f9b8c5bab1b46
Thuotracy <tracyjacobs775@gmail.com>
2022-08-30 10:23:33 +0300

    created superuser

--- photos/models.py
+++ photos/models.py
@@ -13,9 +13,9 @@ class Category(models.Model):
 
 
 class Photo(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     image = models.ImageField(null=False, blank=False)
-    description = models.CharField(max_length=500, null=False, blank=False)
+    description = models.TextField()
 
     def __str__(self):
         return self.description
