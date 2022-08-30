154b3db7f142442c6f546863752f3bd5144e6327
Thuotracy <tracyjacobs775@gmail.com>
2022-08-30 16:14:36 +0300

    looped through the photos

--- photos/views.py
+++ photos/views.py
@@ -5,12 +5,15 @@ from .models import Category, Photo
 
 def gallery(request):
     categories = Category.objects.all()
-    context = {'categories' : categories}
+    photos = Photo.objects.all()
+
+    context = {'categories' : categories, 'photos' : photos}
     return render(request, 'photos/gallery.html', context)
 
 
 def viewPhoto(request, pk):
-    return render(request, 'photos/photo.html')
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photos/photo.html', {'photo': photo})
 
 
 def addPhoto(request):
