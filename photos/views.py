7c686dc23cfdc4af489410918395911b0e94d7b6
Thuotracy <tracyjacobs775@gmail.com>
2022-08-30 15:37:52 +0300

    imported our category and photo classes in the views.py

--- photos/views.py
+++ photos/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from .models import Category, Photo
 
 # Create your views here.
 
 def gallery(request):
+    categories = Category.objects.all()
+    context = {'categories' : categories}
     return render(request, 'photos/gallery.html')
+
 
 def viewPhoto(request, pk):
     return render(request, 'photos/photo.html')
+    
 
 def addPhoto(request):
     return render(request, 'photos/add.html')        
