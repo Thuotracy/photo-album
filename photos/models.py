bdb61800572d81e1440f4418847d2e10ff9844ab
Thuotracy <tracyjacobs775@gmail.com>
2022-08-30 10:11:23 +0300

    created a photo model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
@@ -8,3 +9,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self().name
+
+
+
+class Photo(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    image = models.ImageField(null=False, blank=False)
+    description = models.CharField(max_length=500, null=False, blank=False)
+
+    def __str__(self):
+        return self.description
