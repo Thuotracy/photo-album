d7979e3210e7f85d3c119f2f448398b889960e86
Thuotracy <tracyjacobs775@gmail.com>
2022-08-30 12:39:10 +0300

    created a staic folder

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Photo, Category
+
+admin.site.register(Category)
+admin.site.register(Photo)
--- photos/models.py
+++ photos/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
 
 
 class Photo(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     image = models.ImageField(null=False, blank=False)
     description = models.TextField()
 
