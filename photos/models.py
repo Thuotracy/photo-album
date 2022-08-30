1197b8a7733d0a86c9d0a306c07b9483a2bf77b9
Thuotracy <tracyjacobs775@gmail.com>
2022-08-30 15:32:07 +0300

    collected the staticfiles

--- photos/models.py
+++ photos/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
 
     def __str__(self):
-        return self().name
+        return self.name
 
 
 
--- photoshare/settings.py
+++ photoshare/settings.py
@@ -125,6 +125,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
